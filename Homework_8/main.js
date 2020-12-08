07ff20ae6a162e5118e5289b4f3f2606333b4874
Bradybaldwin <bradybaldwin97@gmail.com>
2020-12-08 02:03:00 -0500

    Finished project detial page and all bootstrap code

--- Homework_8/main.js
+++ Homework_8/main.js
@@ -1,3 +1,12 @@
 function showMail() {
     alert("bradybaldwin97@gmail.com")
 }
+
+window.onscroll = function() {progShow()};
+
+function progShow() {
+    var scrollPos = document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var postScroll = (scrollPos / height) * 100;
+    document.getElementById("thisBar").style.width = postScroll + "%";
+}
